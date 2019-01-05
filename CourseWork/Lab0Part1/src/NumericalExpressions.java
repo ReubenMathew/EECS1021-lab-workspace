6486cb956a9da079cc7141f420e22df6a44182c0
ninan-reuben-216315509 <reubenmn@my.yorku.ca>
2019-01-05 17:51:20 -0500

    init commit

---
+++ CourseWork/Lab0Part1/src/NumericalExpressions.java
@@ -0,0 +1,11 @@
+
+public class NumericalExpressions {
+
+	public static void main(String[] args) {
+	//comment not executed by computer
+	System.out.println(15+4);
+	System.out.println(15-4);
+	System.out.println(15*4);
+	System.out.println(15/4);
+	}
+}
---
+++ CourseWork/Lab0Part1/src/SimpleConsoleOutputs.java
@@ -0,0 +1,10 @@
+
+public class SimpleConsoleOutputs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Now is Winter 2019");
+		System.out.println("Hello EECS1021!");
+	}
+
+}
