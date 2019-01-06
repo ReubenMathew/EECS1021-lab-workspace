04eab4f98472a8b4ca9b7fe4707454adf6b30511
Reuben Mathew <intelphysic@gmail.com>
2019-01-05 23:28:26 -0500

    added missing class

--- Lab0Part1/src/SwapTwoStringsWrong.java
+++ Lab0Part1/src/SwapTwoStringsWrong.java
@@ -1,5 +1,5 @@
 
-public class SwapTwoStrings {
+public class SwapTwoStringsWrong {
 
 	public static void main(String[] args) {
 		String person1 = "Alan";
@@ -9,9 +9,8 @@ public class SwapTwoStrings {
 		System.out.print("person1 is " + person1+ " and ");
 		System.out.println("person2 is " +person2+".");
 		
-		String temp = person1;
 		person1 = person2;
-		person2 = temp;
+		person2 = person1;
 		
 		System.out.print("After the swap:");
 		System.out.print("person1 is " + person1+ " and ");
